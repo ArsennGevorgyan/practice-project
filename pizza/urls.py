160e99f0bd6650524e5d0278eb21a602bee4b131
Hayk <hayknersesyanaj@gmail.com>
2023-12-01 20:01:36 +0400

    burger table implementation

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from pizza.views import pizza, about_us
+from pizza.views import pizza, burger, about_us
 
 urlpatterns = [
     path("", pizza, name="pizzas"),
+    path("burgers/", burger, name="burgers"),
     path("about-us/", about_us, name="about_us")
 ]
